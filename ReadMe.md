e65512a7f782b913234a62651a759999b64d7142
Gian Cedrick G. Epilan <epilred@icloud.com>
2020-10-08 14:28:43 +0800

    sample js calendar lib

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# Fun Javascript Libraries
+
+A bunch of experimental scripts to be used for other projects.
+
+Sample in html files:
+<script src"https://rawgit.com/gOps132/fun_js_libs/${commit}/${dir}/${script}"></script>
---
+++ calendar_lib/callib_context.js
@@ -0,0 +1,19 @@
+/* A bunch of datastructures containing the date */
+class Callib {
+    constructor() {
+        
+    }
+
+    helloworld()
+    {
+        console.log("it works!");
+        return "hello library dude";
+    }
+
+    getArrayOfMonths();
+    getBeforeCurrentDate();
+    getAfterCurrentDate();
+
+    // private properties
+    Date = new Date;
+}
